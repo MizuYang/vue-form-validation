ab1a1e0ba56c6b3d19ae15a2086662801f083d8a
Mizu <psdk55052004@gmail.com>
2023-09-19 16:49:58 +0800

    新增 veeValidate: 信箱驗證

---
+++ src/components/demo1/items/Item1.vue
@@ -0,0 +1,29 @@
+<template>
+  <Form v-slot="{ errors }">
+    <Field
+      class="form-control"
+      id="item1"
+      name="email"
+      type="email"
+      rules="required|email"
+      placeholder="xxx@example.com"
+    />
+    <!-- 錯誤回饋 -->
+    <i class="text-danger text-14 ps-1"
+       :class="{'d-none':!Object.keys(errors).length}">
+      <ErrorMessage name="email"/>
+    </i>
+  </Form>
+</template>
+
+<script setup>
+import {
+  Field,
+  Form,
+  ErrorMessage
+} from '@/composables/demo1/items/item1/veeValidate.js'
+</script>
+
+<style lang='scss' scope>
+
+</style>
---
+++ src/composables/demo1/items/item1/veeValidate.js
@@ -0,0 +1,19 @@
+import { Field, Form, ErrorMessage, defineRule, configure } from 'vee-validate'
+import { required, email } from '@vee-validate/rules'
+import { localize, setLocale } from '@vee-validate/i18n'
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json'
+
+defineRule('required', required)
+defineRule('email', email)
+
+configure({
+  generateMessage: localize({ zh_TW: zhTW }),
+  validateOnInput: true
+})
+setLocale('zh_TW')
+
+export {
+  Field,
+  Form,
+  ErrorMessage
+}
