b77ea63f11306f077cc186a7f3d1220b15c9ebe3
Mizu <psdk55052004@gmail.com>
2023-09-20 10:07:18 +0800

    新增 Demo1/veeValidate: between

---
+++ src/components/demo1/items/Item4.vue
@@ -0,0 +1,29 @@
+<template>
+  <Form v-slot="{ errors }">
+    <Field
+      class="form-control"
+      id="item3"
+      name="輸入框"
+      type="text"
+      rules="between:1,10"
+      placeholder="請輸入英文或數字"
+    />
+    <!-- 錯誤回饋 -->
+    <i class="text-danger text-14 ps-1"
+       :class="{'d-none':!Object.keys(errors).length}">
+      <ErrorMessage name="輸入框"/>
+    </i>
+  </Form>
+</template>
+
+<script setup>
+import {
+  Field,
+  Form,
+  ErrorMessage
+} from '@/composables/demo1/items/item1/veeValidate.js'
+</script>
+
+<style lang='scss' scope>
+
+</style>
--- src/views/Demo1.vue
+++ src/views/Demo1.vue
@@ -17,8 +17,13 @@
               :name="demo.name"
               :content="demo.content" />
 
+      <!-- 顯示使用的規則 -->
+      <template v-if="demo?.rules">
+        <Rules :rules="demo.rules" />
+      </template>
       <!-- 每個驗證 demo 的元件 -->
       <component :is="demo.component"></component>
+
     </section>
 
   </main>
@@ -28,9 +33,11 @@
 import { reactive } from 'vue'
 import Form from '@/components/Form.vue'
 import Header from '@/components/Header.vue'
+import Rules from '@/components/demo1/Rules.vue'
 import Item1 from '@/components/demo1/items/Item1.vue'
 import Item2 from '@/components/demo1/items/Item2.vue'
 import Item3 from '@/components/demo1/items/Item3.vue'
+import Item4 from '@/components/demo1/items/Item4.vue'
 
 const states = reactive([
   {
@@ -47,6 +54,12 @@ const states = reactive([
     name: 'alpha_num',
     content: '字段可以包含字母字符或數字',
     component: Item3
+  },
+  {
+    name: 'between',
+    content: '字段的數值必須在指定的最小值和最大值之間',
+    rules: 'rules="between:1,10"',
+    component: Item4
   }
 ])
 
