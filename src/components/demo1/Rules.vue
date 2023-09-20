55a8c88d860d883cd4672770cac3e9b0c0b482cf
Mizu <psdk55052004@gmail.com>
2023-09-20 10:06:25 +0800

    新增 Demo1/veeValidate: Rules.vue

---
+++ src/components/demo1/Rules.vue
@@ -0,0 +1,16 @@
+<template>
+  <p class="bg-dark mb-3 mx-3 my-1 ps-3 py-1">
+    <code style="color:#fff !important">{{ rules }}</code>
+  </p>
+</template>
+
+<script setup>
+import { toRefs, defineProps } from 'vue'
+
+const props = defineProps(['rules'])
+const { rules } = toRefs(props)
+</script>
+
+<style lang='scss' scope>
+
+</style>
