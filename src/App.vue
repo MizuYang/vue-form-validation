861fa8d3fa867f47b034742915112c0ef0be52cd
Mizu <psdk55052004@gmail.com>
2023-09-23 11:03:58 +0800

    修改 Demo1/veeValidate: RouterLink 資料渲染

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">demo1</router-link>
+  <nav class="position-sticky top-0 z100 w-100 p-3"
+       style="background-color:#ededed;">
+    <ul class="d-flex">
+      <li v-for="item in routerLink" :key="item.name"
+          class="me-2">
+        <RouterLink :to="item.path">
+          {{ item.name }}
+        </RouterLink>
+      </li>
+    </ul>
   </nav>
 
   <main class="container p-3">
     <router-view />
   </main>
 </template>
+
+<script setup>
+import { reactive } from 'vue'
+const routerLink = reactive([
+  {
+    path: '/',
+    name: 'VeeValidate'
+  },
+  {
+    path: '/TaiwanIdValidator',
+    name: 'TaiwanIdValidator'
+  }
+])
+</script>
 
 <style lang="scss">
 @import "@/assets/stylesheets/all.scss";
