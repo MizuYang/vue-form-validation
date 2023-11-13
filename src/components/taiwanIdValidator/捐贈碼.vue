6d441e1df0ae6ddc5b2c8f778c326e9de490d6f3
Mizu <psdk55052004@gmail.com>
2023-11-13 16:32:44 +0800

    新增 taiwanIdValidator: 捐贈碼

---
+++ src/components/taiwanIdValidator/捐贈碼.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- 捐贈碼 -->
+  <section class="area p-3">
+    <label for="中文姓名" class="form-label text-20">
+      {{ item.name }}
+      <input type="text"
+             class="form-control"
+             :class="{
+              'is-valid': item.isValid === 1,
+              'is-invalid': item.isValid === 2,
+             }"
+             id="中文姓名"
+             @[validateEvent]="$emit('validateRules', $event,'捐贈碼')"
+             :placeholder="item.placeHolder">
+    </label>
+  </section>
+</template>
+
+<script setup>
+import { toRefs, defineProps, defineEmits } from 'vue'
+
+// props, emit
+const props = defineProps(['item', 'validateEvent'])
+const { item, validateEvent } = toRefs(props)
+defineEmits(['validateRules'])
+</script>
+
+<style lang='scss' scope>
+
+</style>
