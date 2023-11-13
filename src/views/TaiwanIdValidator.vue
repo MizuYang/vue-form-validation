d80f9a8631b9ac63a10091942d96340fffceaaf4
Mizu <psdk55052004@gmail.com>
2023-11-13 15:52:35 +0800

    新增 taiwanIdValidator: 安裝 taiwan-id-validator.js、測試用資料結構

--- src/views/TaiwanIdValidator.vue
+++ src/views/TaiwanIdValidator.vue
@@ -1,45 +1,220 @@
 <template>
   <header>
-    <h2 class="text-20">Taiwan-id-validator</h2>
+    <h2 class="text-20">
+      Taiwan-id-validator
+      <a href="https://hugo.digitalgo.synology.me/2022/04/%E8%BA%AB%E5%88%86%E8%AD%89%E5%AD%97%E8%99%9F%E6%96%B0%E5%BC%8F%E5%B1%85%E7%95%99%E8%AD%89%E8%99%9F%E8%88%8A%E5%BC%8F%E5%B1%85%E7%95%99%E8%AD%89%E8%99%9F%E5%8F%8A%E7%B5%B1%E4%B8%80%E7%B7%A8%E8%99%9F%E7%94%A2%E7%94%9F%E5%99%A8%E6%9A%A8%E9%A9%97%E8%AD%89%E5%99%A8/"
+        target="_blank"
+        class="text-14 border-bottom border-primary ms-5 px-2">
+        測試資料產生器
+      </a>
+    </h2>
+
+     <!-- 設定配置 -->
+    <div class="d-flex my-3">
+      <div class="form-check">
+        <label for="一輸入就驗證" class="form-check-label">
+          <input type="radio" name="validateEventName"
+                class="form-check-input" id="一輸入就驗證"
+                value="一輸入就驗證"
+                v-model="validateEventName">
+          一輸入就驗證
+        </label>
+      </div>
+      <div class="form-check mx-3">
+        <label for="離開焦點才驗證" class="form-check-label">
+          <input type="radio" name="validateEventName"
+                class="form-check-input" id="離開焦點才驗證"
+                value="離開焦點才驗證"
+                v-model="validateEventName">
+          離開焦點才驗證
+        </label>
+      </div>
+      <div class="form-check">
+        <label for="手動驗證" class="form-check-label">
+          <input type="radio" name="validateEventName"
+                class="form-check-input" id="手動驗證"
+                value="手動驗證"
+                v-model="validateEventName">
+          手動驗證
+        </label>
+      </div>
+    </div>
   </header>
 
   <form class="mt-3">
-   <!-- 中文姓名 -->
-   <ChineseName />
+    <!-- 每個demo區塊 -->
+    <div v-for="item in data" :key="item.name">
+      <component :is="item.component"
+                 v-model="item.vModel"
+                 :item="item"
+                 :validateEventName="validateEventName"
+                 @validateRules="validateRules"></component>
+    </div>
 
-   <!-- 英文姓名 -->
-   <EnglishName />
-
-   <!-- 身分證字號 -->
-   <IdentityCard />
-
-   <!-- 手機載具 -->
-   <MobileCarrier />
-
-   <!-- 生日 -->
-   <Birthday />
-
-   <!-- 信箱 -->
-   <Email />
-
-   <!-- 手機號碼 -->
-   <PhoneNumber />
-
-   <!-- 選擇縣市 -->
-   <SelectCity />
+    <button type="button" @click="validate">
+      驗證
+    </button>
   </form>
 </template>
 
 <script setup>
-import ChineseName from '@/components/taiwanIdValidator/ChineseName.vue'
-import EnglishName from '@/components/taiwanIdValidator/EnglishName.vue'
-import IdentityCard from '@/components/taiwanIdValidator/IdentityCard.vue'
-import MobileCarrier from '@/components/taiwanIdValidator/MobileCarrier.vue'
-import Birthday from '@/components/taiwanIdValidator/Birthday.vue'
-import Email from '@/components/taiwanIdValidator/Email.vue'
-import PhoneNumber from '@/components/taiwanIdValidator/PhoneNumber.vue'
-import SelectCity from '@/components/taiwanIdValidator/SelectCity.vue'
+import { ref, reactive, defineAsyncComponent } from 'vue'
+import {
+  isGuiNumberValid, // 統一編號
+  isNationalIdentificationNumberValid, // 身分證字號
+  isResidentCertificateNumberValid, // 居留證編號
+  isNewResidentCertificateNumberValid, // 新式居留證編號
+  isOriginalResidentCertificateNumberValid, // 舊式居留證編號
+  isCitizenDigitalCertificateNumberValid, // 自然人憑證
+  isEInvoiceCellPhoneBarcodeValid, // 手機條碼
+  isEInvoiceDonateCodeValid, // 捐贈碼
+  isCreditCardNumberValid // 信用卡
+} from 'taiwan-id-validator'
 
+// data
+const validateEventName = ref('一輸入就驗證')
+const data = reactive([
+  {
+    name: '統一編號',
+    fileName: '統一編號',
+    isValid: 0, // 0:初始直初始值 1:成功 2:失敗
+    component: null,
+    rule: isGuiNumberValid,
+    // vModel: 統一編號,
+    placeHolder: '請輸入 統一編號',
+    testString: ['94609767', '09702853', '15859493']
+  },
+  {
+    name: '身分證字號',
+    fileName: '身分證字號',
+    isValid: 0, // 0:初始直初始值 1:成功 2:失敗
+    component: null,
+    rule: isNationalIdentificationNumberValid,
+    // vModel: 身分證字號,
+    placeHolder: '請輸入 身分證字號',
+    testString: []
+  },
+  {
+    name: '居留證編號',
+    fileName: '居留證編號',
+    isValid: 0, // 0:初始直初始值 1:成功 2:失敗
+    component: null,
+    rule: isResidentCertificateNumberValid,
+    // vModel: 居留證編號,
+    placeHolder: '請輸入 居留證編號',
+    testString: []
+  },
+  {
+    name: '新式居留證編號',
+    fileName: '新式居留證編號',
+    isValid: 0, // 0:初始直初始值 1:成功 2:失敗
+    component: null,
+    rule: isNewResidentCertificateNumberValid,
+    // vModel: 新式居留證編號,
+    placeHolder: '請輸入 新式居留證編號',
+    testString: []
+  },
+  {
+    name: '舊式居留證編號',
+    fileName: '舊式居留證編號',
+    isValid: 0, // 0:初始直初始值 1:成功 2:失敗
+    component: null,
+    rule: isOriginalResidentCertificateNumberValid,
+    // vModel: 舊式居留證編號,
+    placeHolder: '請輸入 舊式居留證編號',
+    testString: []
+  },
+  {
+    name: '自然人憑證',
+    fileName: '自然人憑證',
+    isValid: 0, // 0:初始直初始值 1:成功 2:失敗
+    component: null,
+    rule: isCitizenDigitalCertificateNumberValid,
+    // vModel: 自然人憑證,
+    placeHolder: '請輸入 自然人憑證',
+    testString: []
+  },
+  {
+    name: '手機條碼',
+    fileName: '手機條碼',
+    isValid: 0, // 0:初始直初始值 1:成功 2:失敗
+    component: null,
+    rule: isEInvoiceCellPhoneBarcodeValid,
+    // vModel: 手機條碼,
+    placeHolder: '請輸入 手機條碼',
+    testString: []
+  },
+  {
+    name: '捐贈碼',
+    fileName: '捐贈碼',
+    isValid: 0, // 0:初始直初始值 1:成功 2:失敗
+    component: null,
+    rule: isEInvoiceDonateCodeValid,
+    // vModel: 捐贈碼,
+    placeHolder: '請輸入 捐贈碼',
+    testString: []
+  },
+  {
+    name: '信用卡',
+    fileName: '信用卡',
+    isValid: 0, // 0:初始直初始值 1:成功 2:失敗
+    component: null,
+    rule: isCreditCardNumberValid,
+    // vModel: 信用卡,
+    placeHolder: '請輸入 信用卡',
+    testString: []
+  }
+])
+const form = reactive({
+  統一編號: ref(''),
+  身分證字號: ref(''),
+  居留證編號: ref(''),
+  新式居留證編號: ref(''),
+  舊式居留證編號: ref(''),
+  自然人憑證: ref(''),
+  手機條碼: ref(''),
+  捐贈碼: ref(''),
+  信用卡: ref('')
+})
+getComponent()
+
+function getComponent () {
+  data.forEach(item => {
+    item.component = defineAsyncComponent(() => import(`@/components/taiwanIdValidator/${item.fileName}.vue`))
+  })
+}
+function validateRules (e, ruleName) {
+  console.log(e.target.value)
+
+  const value = e.target.value
+
+  // 更新表單欄位的值
+  form[ruleName] = value
+
+  if (validateEventName.value === '手動驗證') return
+
+  const rule = data.filter(item => item.name === ruleName)[0].rule
+
+  // 更新 data 驗證的結果
+  data.forEach(item => {
+    if (item.name === ruleName) item.isValid = rule(value) ? 1 : 2
+  })
+}
+function validate () {
+  data.forEach(item => {
+    const ruleName = item.name
+    const value = form[ruleName]
+
+    if (value) {
+      const rule = item.rule
+      const isValid = rule(value)
+
+      item.isValid = isValid ? 1 : 2
+    } else {
+      item.isValid = 2
+    }
+  })
+}
 </script>
 
 <style lang='scss' scope>
